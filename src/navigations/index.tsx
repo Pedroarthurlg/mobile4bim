31896d9f81072037bea4b800b3650987657a3312
Pedroarthurlg <pedroarthurlg@gmail.com>
2022-09-08 15:25:07 -0300

    ok

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -3,13 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoginNavigation from "./login.navigation";
 import DrawerNavigation from "./drawer.navigation";
 import { useAuth } from "../hook/auth";
-import TabNavigation from "./tab.navigation";
 
 export default function Navigation() {
   const { access_token } = useAuth();
   return (
-  <NavigationContainer>
-    {access_token ? <TabNavigation /> : <LoginNavigation />}
-  </NavigationContainer>
+    <NavigationContainer>
+      {access_token ? <DrawerNavigation /> : <LoginNavigation />}
+    </NavigationContainer>
   );
- }
+}
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,11 +1,10 @@
 export default {
   black: '#000000',
-  gray: "#808080",
   white: '#ffffff',
   primary: '#00ff00',
   primaryLight: '#CFD8DC',
   secondary: '#0000ff',
-  secondaryLight: '#4d4dff',
-  third: '#812080',
+  secondaryLight: '#ff0000',
+  third: '#ffff00',
   thirdLight: '#ff00ff'
 }
